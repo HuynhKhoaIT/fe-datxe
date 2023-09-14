4ecb604fa29206da91edaa4b011adefa4ae94c8d
vantaihvp <vantai.itc@gmail.com>
2023-09-14 22:40:46 +0700

    chuyen gia

---
+++ src/app/components/dashboard/order/orders.tsx
@@ -0,0 +1,51 @@
+'use client';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import Link from 'next/link';
+const Orders = ({
+    productId,
+    name,
+    price,
+    thumbnail,
+}: {
+    productId: number;
+    name: string;
+    price: number;
+    thumbnail: any;
+}) => {
+    return (
+        <div className="shop-item">
+            <div className="shop-item-img">
+                <span className="shop-item-sale">Sale</span>
+                <img src={thumbnail} alt="" />
+                <div className="shop-item-meta">
+                    <a href="#">
+                        <i className="far fa-heart"></i>
+                    </a>
+                    <a href="#">
+                        <i className="far fa-eye"></i>
+                    </a>
+                    <a href="#">
+                        <i className="far fa-shopping-cart"></i>
+                    </a>
+                </div>
+            </div>
+            <div className="shop-item-info">
+                <div className="shop-item-rate">
+                    <FontAwesomeIcon icon={faStar} />
+                    <FontAwesomeIcon icon={faStar} />
+                    <FontAwesomeIcon icon={faStar} />
+                    <FontAwesomeIcon icon={faStar} />
+                    <FontAwesomeIcon icon={faStar} />
+                </div>
+                <Link href={`/san-pham/${productId}`}>
+                    <h4 className="shop-item-title">{name}</h4>
+                </Link>
+                <div className="shop-item-price">
+                    <del>{price.toLocaleString()}đ</del> {price.toLocaleString()}đ
+                </div>
+            </div>
+        </div>
+    );
+};
+export { Orders };
