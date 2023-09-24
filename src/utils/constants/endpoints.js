dd6bfbe6aa83d822bb18cfc24528e44ee5144c21
vantaihvp <vantai.itc@gmail.com>
2023-09-24 21:52:55 +0700

    update api link

--- src/utils/constants/endpoints.js
+++ src/utils/constants/endpoints.js
@@ -4,5 +4,5 @@ export const GET_PRODUCT_ENDPOINT = `${process.env.apiGuest}/products`;
 export const GET_MY_ACCOUNT_ENDPOINT = `${process.env.apiUser}/my-account`;
 export const GET_CATEGORY_ENDPOINT = `${process.env.apiGuest}/product-category`;
 export const GET_GARAGE_ENDPOINT = `${process.env.apiGuest}/garages`;
-export const POST_LOGIN_ENDPOINT = `${process.env.apiGuest}/login`;
-export const POST_REGISTER_ENDPOINT = `${process.env.apiGuest}/register`;
+export const POST_LOGIN_ENDPOINT = `${process.env.api}/login`;
+export const POST_REGISTER_ENDPOINT = `${process.env.api}/register`;
