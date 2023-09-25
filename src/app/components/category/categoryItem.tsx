0826b32f90686e27034e6868e8066b65f5ee9e76
HuynhKhoaIT <114349590+HuynhKhoaIT@users.noreply.github.com>
2023-09-25 13:18:58 +0700

    chi tiết sản phẩm

--- src/app/components/category/categoryItem.tsx
+++ src/app/components/category/categoryItem.tsx
@@ -1,5 +1,3 @@
-// 'use client';
-
 import Link from 'next/link';
 import { ICategory } from '@/interfaces/category';
 const CategoryItem = ({ key, category }: { key: number; category: ICategory }) => {
