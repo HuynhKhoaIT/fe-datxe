e470427d4b6f0375cadf193d8283b8c0c16dd49d
HuynhKhoaIT <>
2023-12-12 12:09:27 +0700

    gio hang

--- src/app/gio-hang/page.tsx
+++ src/app/gio-hang/page.tsx
@@ -1,17 +1,3 @@
-'use client';
-import React, { FormEvent, Suspense, useEffect, useRef, useState } from 'react';
-import { checkOutCart } from '@/utils/order';
-import dayjs from 'dayjs';
-import { useSession } from 'next-auth/react';
-import { Grid, Modal, TextInput, Box, Select, Button, Group, Table, LoadingOverlay, Card, Avatar } from '@mantine/core';
-import { ActionIcon, rem } from '@mantine/core';
-import { notifications } from '@mantine/notifications';
-import { IconClock12, IconTrash, IconBan, IconChevronRight, IconMinus, IconPlus } from '@tabler/icons-react';
-import CartItemRow from './CartItemRow';
-import InfoCustomer from './InfoCustomer';
-import InfoCar from './InfoCar';
-import InfoCart from './InfoCart';
-import InfoDate from './InfoDate';
 import CartComponent from './CartComponent';
 import { getCarsSsr } from '@/utils/car';
 import { getMyAccount } from '@/utils/user';
