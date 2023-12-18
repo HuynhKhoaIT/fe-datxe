573ee15f8250e7d96b1c0b5b99df3f1d7367fa1c
HuynhKhoaIT <>
2023-12-18 08:36:07 +0700

    utc

--- src/app/domain/EventCalendar.ts
+++ src/app/domain/EventCalendar.ts
@@ -12,6 +12,9 @@ export interface IEventCalendar {
     orderDetail: any;
 }
 export const mapEventCalendar = (eventCalendar: IOrder, index: number): IEventCalendar => {
+    const dayjs = require('dayjs');
+    const utc = require('dayjs/plugin/utc');
+    dayjs.extend(utc);
     return {
         _id: (index + 1).toString(),
         title: eventCalendar?.garage?.name || '',
