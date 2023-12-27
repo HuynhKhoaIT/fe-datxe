c32bda6bec1fa3721421c52d74358ea8328f1952
vantaihvp <vantai.itc@gmail.com>
2023-12-27 22:15:36 +0700

    update api product category

---
+++ src/app/api/product-category/route.ts
@@ -0,0 +1,35 @@
+import prisma from '@/app/libs/prismadb';
+import { getServerSession } from 'next-auth/next';
+import { NextResponse } from 'next/server';
+import { authOptions } from '../auth/[...nextauth]/route';
+
+export async function GET(request: Request) {
+    try {
+        const session = await getServerSession(authOptions);
+        if (session?.user?.token) {
+            const productCategory = await prisma.productCategory.findMany({
+                take: 10,
+            });
+            return NextResponse.json(productCategory);
+        }
+        throw new Error('Chua dang nhap');
+    } catch (error: any) {
+        return new NextResponse(error.message, { status: 500 });
+    }
+}
+
+export async function POST(request: Request) {
+    try {
+        const json = await request.json();
+        const productCategory = await prisma.productCategory.create({
+            data: json,
+        });
+
+        return new NextResponse(JSON.stringify(productCategory), {
+            status: 201,
+            headers: { 'Content-Type': 'application/json' },
+        });
+    } catch (error: any) {
+        return new NextResponse(error.message, { status: 500 });
+    }
+}
