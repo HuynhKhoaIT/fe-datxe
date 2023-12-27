42252ff5144ab19c27d6a597cb2e684fe95c4d84
HuynhKhoaIT <>
2023-12-28 00:55:05 +0700

    fix

--- src/app/admin/products/create/page.tsx
+++ src/app/admin/products/create/page.tsx
@@ -1,29 +1,28 @@
-"use client";
 import { Box, Space } from "@mantine/core";
 import Typo from "@/app/components/elements/Typo";
 import styles from "../index.module.scss";
 import ProductForm from "./ProductForm";
 import { useEffect, useState } from "react";
-export default function ProductSavePage() {
-  const [categoryOptions, setCategoryOptions] = useState<any>();
-  useEffect(() => {
-    fetch(`/api/product-category`)
-      .then((res) => res.json())
-      .then((data) => {
-        const dataOption = data?.map((item: any) => ({
-          value: item.id.toString(),
-          label: item.title,
-        }));
-        setCategoryOptions(dataOption);
-      });
-  }, []);
+export default async function ProductSavePage() {
+  // const [categoryOptions, setCategoryOptions] = useState<any>();
+  // useEffect(() => {
+  //   fetch(`/api/product-category`)
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       const dataOption = data?.map((item: any) => ({
+  //         value: item.id.toString(),
+  //         label: item.title,
+  //       }));
+  //       setCategoryOptions(dataOption);
+  //     });
+  // }, []);
   return (
     <Box maw={"100%"} mx="auto" className={styles.content}>
       <Typo size="small" type="bold" style={{ color: "var(--theme-color)" }}>
         Thêm sản phẩm
       </Typo>
       <Space h="md" />
-      <ProductForm isEditing={false} categoryOptions={categoryOptions} />
+      <ProductForm isEditing={false} />
     </Box>
   );
 }
