089bb87b4f2c7254857b170f200b9ac3d8b5be6b
vantaihvp <vantai.itc@gmail.com>
2024-01-07 16:07:44 +0700

    update products

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -26,28 +26,64 @@ export async function GET(request: NextRequest, { params }: { params: { id: numb
 
 export async function PUT(request: NextRequest, { params }: { params: { id: number } }) {
     try {
-        const id = params.id;
-        if (!id) {
-            return new NextResponse("Missing 'id' parameter");
+        const session = await getServerSession(authOptions);
+        if (session?.user?.token) {
+            const id = params.id;
+            let catArr: any = [];
+            if (!id) {
+                return new NextResponse("Missing 'id' parameter");
+            }
+            const json = await request.json();
+
+            if (!json.categories) {
+                return new NextResponse("Missing 'categoryId' parameter");
+            } else {
+                json.categories.forEach(function (id: number) {
+                    catArr.push({
+                        assignedBy: session?.user?.name ?? '',
+                        assignedAt: new Date(),
+                        category: {
+                            connect: {
+                                id: parseInt(id.toString()),
+                            },
+                        },
+                    });
+                });
+            }
+            const product = await prisma.product.findUnique({
+                where: {
+                    id: parseInt(id.toString()),
+                },
+            });
+            const updatedPost = await prisma.product.update({
+                where: {
+                    id: parseInt(id.toString()),
+                },
+                data: {
+                    name: json.name,
+                    price: json.price,
+                    salePrice: json.salePrice,
+                    productId: json.productId ?? 0,
+                    description: json.description ?? '',
+                    timeSaleStart: json.timeSaleStart ?? null,
+                    timeSaleEnd: json.timeSaleEnd ?? null,
+                    quantity: json.quantity ?? 0,
+                    images: json.images ?? null,
+                    metaDescription: json.metaDescription ?? null,
+                    status: json.status,
+                    createdBy: 1,
+                    garageId: 0,
+                    categories: {
+                        create: catArr,
+                    },
+                },
+            });
+
+            return new NextResponse(JSON.stringify(product), {
+                status: 201,
+                headers: { 'Content-Type': 'application/json' },
+            });
         }
-        const json = await request.json();
-        const product = await prisma.product.findUnique({
-            where: {
-                id: parseInt(id.toString()),
-            },
-        });
-
-        const updatedPost = await prisma.product.update({
-            where: {
-                id: parseInt(id.toString()),
-            },
-            data: json,
-        });
-
-        return new NextResponse(JSON.stringify(product), {
-            status: 201,
-            headers: { 'Content-Type': 'application/json' },
-        });
     } catch (error: any) {
         return new NextResponse(error.message, { status: 500 });
     }
@@ -59,9 +95,12 @@ export async function DELETE(request: NextRequest, { params }: { params: { id: n
         return new NextResponse("Missing 'id' parameter");
     }
 
-    const deletePost = await prisma.product.delete({
+    const product = await prisma.product.update({
         where: {
             id: parseInt(id.toString()),
+        },
+        data: {
+            status: 'DELETE',
         },
     });
 
