28e6064408e75aaeed8b2ded9e4cd625e17a9511
HuynhKhoaIT <>
2024-02-23 21:45:13 +0700

    khoa dev

---
+++ src/app/layout/mobile/landing/Introducation/index.tsx
@@ -0,0 +1,36 @@
+import Typo from "@/app/components/elements/Typo";
+import { Button } from "@mantine/core";
+import IconPlay from "@/assets/icons/play.svg";
+import styles from "./index.module.scss";
+const Introducation = () => {
+  return (
+    <div className={styles.wrapperBox}>
+      <Typo
+        type="bold"
+        style={{
+          color: "var(--title-white)",
+          fontSize: "32px",
+          textAlign: "center",
+        }}
+      >
+        Đặt lịch sửa xe tại 63 tỉnh thành
+      </Typo>
+      <Typo
+        size="primary"
+        style={{ color: "var(--title-white)", textAlign: "center" }}
+      >
+        Không còn phải xếp hàng đợi sửa xe, biết trước giá dịch vụ, tiết kiệm
+      </Typo>
+      <Button
+        leftSection={<img src={IconPlay.src} />}
+        radius="lg"
+        color="#6C00FF"
+        h={38}
+        w={158}
+      >
+        Xem video giới thiệu
+      </Button>
+    </div>
+  );
+};
+export default Introducation;
