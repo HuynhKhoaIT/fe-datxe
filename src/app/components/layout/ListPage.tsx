8800109a721b8b220fe9ff60a54f6197498d2889
HuynhKhoaIT <>
2024-03-11 14:45:22 +0700

    layout

--- src/app/components/layout/ListPage.tsx
+++ src/app/components/layout/ListPage.tsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import styles from "./ListPage.module.scss";
 import { Box } from "@mantine/core";
+import Typo from "../elements/Typo";
+import { IconFilter } from "@tabler/icons-react";
 
 function ListPage({
   searchForm,
@@ -11,15 +13,31 @@ function ListPage({
   children,
   title,
   style,
-  padding = 30,
+  padding = 0,
+  titleTable = false,
 }: any) {
   return (
     <div className={styles.baseListPage} style={{ padding: padding, ...style }}>
       <div className={styles.title}>{title}</div>
       <div className={styles.baseListPageList}>
-        <div className={styles.searchForm}> {searchForm}</div>
-        <div className={styles.actionBar}>{actionBar}</div>
-        <div className={styles.baseTable}>{baseTable}</div>
+        <div className={styles.searchForm}>{searchForm}</div>
+        {actionBar && <div className={styles.actionBar}>{actionBar}</div>}
+        <div className={styles.baseTable}>
+          {titleTable && (
+            <div style={{ borderBottom: "1px solid #eeeeee" }}>
+              <Typo
+                size="18px"
+                type="bold"
+                style={{ color: "var(--primary-orange)", padding: "16px 30px" }}
+              >
+                <IconFilter size={22} />
+                Danh sách
+              </Typo>
+            </div>
+          )}
+
+          <div className={styles.table}>{baseTable}</div>
+        </div>
       </div>
       {children}
     </div>
