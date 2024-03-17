81209930397955d1dedd2e83af3407d96ab9f18c
vantaihvp <vantai.itc@gmail.com>
2024-03-17 17:11:50 +0700

    .

--- src/app/api/car/route.ts
+++ src/app/api/car/route.ts
@@ -3,14 +3,10 @@ import { getServerSession } from 'next-auth';
 import { NextRequest, NextResponse } from 'next/server';
 import { authOptions } from '../auth/[...nextauth]/route';
 import { getGarageIdByDLBDID } from '@/app/libs/prisma/garage';
-interface carData {
-    numberPlates: string;
-    // abc: string;
-}
+
 export async function GET(request: NextRequest) {
     try {
         const session = await getServerSession(authOptions);
-
         if (session) {
             let garageId = await getGarageIdByDLBDID(Number(session.user?.garageId));
             const { searchParams } = new URL(request.url);
