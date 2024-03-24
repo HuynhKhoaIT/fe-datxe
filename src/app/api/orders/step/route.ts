daf6540b9c5fb929c5d8faa6483c07643f6bac19
vantaihvp <vantai.itc@gmail.com>
2024-03-24 21:49:56 +0700

    fix api update step

---
+++ src/app/api/orders/step/route.ts
@@ -0,0 +1,22 @@
+import { updateOrderStep } from '@/app/libs/prisma/order';
+import { getServerSession } from 'next-auth';
+import { NextRequest, NextResponse } from 'next/server';
+import { authOptions } from '../../auth/[...nextauth]/route';
+
+export async function POST(request: Request) {
+    try {
+        const json = await request.json();
+        const session = await getServerSession(authOptions);
+        if (session) {
+            const orderId = json.id;
+            const orderStep = json.step;
+            const order = await updateOrderStep(orderId, orderStep);
+            return new NextResponse(JSON.stringify(order), {
+                status: 201,
+                headers: { 'Content-Type': 'application/json' },
+            });
+        }
+    } catch (error: any) {
+        return new NextResponse(error.message, { status: 500 });
+    }
+}
