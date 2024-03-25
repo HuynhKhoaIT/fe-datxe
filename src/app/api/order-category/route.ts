1dd397dc3a9c7f91fd6e7888fb65690139c5cf66
vantaihvp <vantai.itc@gmail.com>
2024-03-25 22:20:59 +0700

    .

--- src/app/api/order-category/route.ts
+++ src/app/api/order-category/route.ts
@@ -1,5 +1,8 @@
 import prisma from '@/app/libs/prismadb';
+import { getServerSession } from 'next-auth';
 import { NextRequest, NextResponse } from 'next/server';
+import { authOptions } from '../auth/[...nextauth]/route';
+import { getGarageIdByDLBDID } from '@/app/libs/prisma/garage';
 
 export async function GET(request: NextRequest) {
     try {
@@ -18,19 +21,24 @@ export async function GET(request: NextRequest) {
 }
 export async function POST(request: Request) {
     try {
-        const json = await request.json();        
-        const orderCategory = await prisma.orderCategory.create({
-            data: {
-                title: json.title,
-                garageId: json.garageId,
-                status: json.status,
-            },
-        });
+        const json = await request.json();
+        const session = await getServerSession(authOptions);
+        if (session) {
+            let garageId = await getGarageIdByDLBDID(Number(session.user?.garageId));
+            const orderCategory = await prisma.orderCategory.create({
+                data: {
+                    title: json.title,
+                    garageId: garageId,
+                    status: json.status,
+                },
+            });
 
-        return new NextResponse(JSON.stringify(orderCategory), {
-            status: 201,
-            headers: { 'Content-Type': 'application/json' },
-        });
+            return new NextResponse(JSON.stringify(orderCategory), {
+                status: 201,
+                headers: { 'Content-Type': 'application/json' },
+            });
+        }
+        throw new Error('Chua dang nhap');
     } catch (error: any) {
         return new NextResponse(error.message, { status: 500 });
     }
